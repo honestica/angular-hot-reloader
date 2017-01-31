73fb9c232315f71232b90dc253f4b0c4112c680d
Felix <felix@honestica.com>
2017-01-31 09:40:44 +0100

    add typescript

--- example/webpack.common.config.js
+++ example/webpack.common.config.js
@@ -9,13 +9,16 @@ module.exports = {
     filename: 'bundle.js',
     publicPath: '/static/'
   },
+  resolve: {
+      extensions: [ '', '.ts', '.js', '.html' ]
+  },
   resolveLoader: {
       root: nodeModulesPath
   },
-
   module: {
     loaders: [
        { test: /\.js$/, exclude: [/app\/lib/, /node_modules/], loader: 'ng-annotate!babel' },
+       { test: /\.ts$/, exclude: [/app\/lib/, /node_modules/], loader: 'ts' },
        { test: /\.html$/, loader: 'raw' },
        { test: /\.less$/, loader: 'style!css!less' },
        { test: /\.css$/, loader: 'style!css' }
